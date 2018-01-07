0ada8eb2fa634057a6557262295415ccfe69857c
puckey <jonathan@puckey.studio>
2018-01-07 22:35:00 +0100

    index

---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ * @providesModule RNMigrateCrosswalk
+ */
+
+'use strict';
+
+import { NativeModules} from 'react-native';
+
+const { RNMigrateCrosswalk } = NativeModules;
+
+export default RNMigrateCrosswalk;
